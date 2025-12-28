5ef9f25d3275777d0e14d2c1b4394c694a5ec2dc
realler13 <denis.petrochenkov@gmail.com>
2025-12-28 14:25:42 -0800

    Enhance search_command to filter out stopwords from queries and movie titles

--- cli/src/keyword_search.py
+++ cli/src/keyword_search.py
@@ -1,19 +1,32 @@
 import string
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
+    stopwords = load_stopwords()
     results = []
     preprocessed_query = tokenize_text(preprocess_text(query))
+    for token in preprocessed_query[:]:
+        if token in stopwords:
+            preprocessed_query.remove(token)
 
     for movie in movies:
         preprocessed_title = tokenize_text(preprocess_text(movie['title']))
+        for title_word in preprocessed_title[:]:  # Copy with [:]
+            if title_word in stopwords:
+                preprocessed_title.remove(title_word)
+        
+        
         for token in preprocessed_query:
             for title in preprocessed_title:
                 if token in title:
                     results.append(movie)
                     break
+            else:
+                continue
+            break
+
         if len(results) >= limit:
             break
     return results
--- cli/src/search_utils.py
+++ cli/src/search_utils.py
@@ -2,11 +2,32 @@ import json
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
-DATA_PATH = PROJECT_ROOT / "data" / "movies.json"
+MOVIE_PATH = PROJECT_ROOT / "data" / "movies.json"
+STOPWORD_PATH = PROJECT_ROOT / "data" / "stopwords.txt"
 
 DEFAULT_SEARCH_LIMIT = 5
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, "r") as f:
+    with open(MOVIE_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stopwords():
+    print(f"DEBUG: Looking for file at: {STOPWORD_PATH}")
+    
+    p = Path(STOPWORD_PATH)
+    print(f"DEBUG: File exists: {p.exists()}")
+    print(f"DEBUG: File size: {p.stat().st_size} bytes")
+
+    
+    with open(STOPWORD_PATH, 'r') as f:
+        stop_words = f.readlines()
+        print(f"DEBUG: Read {len(stop_words)} lines")
+        print(f"DEBUG: First few lines: {stop_words[:3]}")
+        
+        cleaned_list_of_stopwords = []
+        for word in stop_words:
+            cleaned_list_of_stopwords.append(word.strip().lower())
+        
+        print(f"DEBUG: After cleaning: {cleaned_list_of_stopwords[:3]}")
+    return cleaned_list_of_stopwords
